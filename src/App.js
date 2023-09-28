b18a3030bce15cce2b16e0ad1ca2f71dc95b5071
ruwinda90 <ruwinda90@gmail.com>
2023-09-28 15:11:42 +0530

    Hard reset

--- src/App.js
+++ src/App.js
@@ -1,28 +1,8 @@
-import './App.css';
-import {useDispatch, useSelector} from "react-redux";
-import {actions} from "./store";
-
 function App() {
-
-    const counter = useSelector(state => state.counter); // Bind part of the redux store
-    const dispatch = useDispatch(); // send actions to the store.
-    const increment = () => {
-        dispatch(actions.increment());
-    };
-    const decrement = () => {
-        dispatch(actions.decrement());
-    };
-    const addBy = () => {
-      dispatch(actions.addBy(10));
-    }
 
     return (
         <div>
-            <h1>Counter app</h1>
-            <h2>{counter}</h2>
-            <button onClick={increment}>Increment</button>
-            <button onClick={decrement}>Decrement</button>
-            <button onClick={addBy}>Add 10</button>
+            Hello there
         </div>
     );
 }
