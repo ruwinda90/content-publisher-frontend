463658fa3c7ab436d2ce8296bff2dde3e589f882
ruwinda90 <ruwinda90@gmail.com>
2023-10-04 23:36:18 +0530

    Add basic styles and state handling for Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [username, setUsername] = useState("");
+  const [pwd, setPwd] = useState("");
 
-export default Login
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Handle submit");
+  };
+
+  return (
+    <main className="login">
+      <div className="loginArea">
+        <h1>Login to Proceed</h1>
+        <form className="loginForm" onSubmit={handleSubmit}>
+          <label htmlFor="username">Username</label>
+          <input
+            autoFocus
+            id="username"
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            value={pwd}
+            onChange={(e) => setPwd(e.target.value)}
+          />
+          <button disabled={!username || !pwd}>Submit</button>
+        </form>
+        <Link to="/register">Register</Link>
+      </div>
+    </main>
+  );
+};
+
+export default Login;
