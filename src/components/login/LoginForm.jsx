e254e9f189a39dc154b75ca4f700bd044f875379
ruwinda90 <ruwinda90@gmail.com>
2023-10-07 21:41:27 +0530

    Add LoginForm component with API integration

---
+++ src/components/login/LoginForm.jsx
@@ -0,0 +1,80 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { userLogged } from "../../store/authSlice";
+import { authApi as api } from "../../api/apiRequest";
+import FormPrompt from "../register/FormPrompt";
+
+const LoginForm = () => {
+  const [username, setUsername] = useState("");
+  const [pwd, setPwd] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state?.from?.pathname || "/"; // Navigate user back to wherever he/she was.
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    setErrorMessage("");
+  }, [username, pwd]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await api.post("/authenticate", {
+        email: username,
+        password: pwd,
+      });
+      dispatch(userLogged({ accessToken: response.data.response.token }));
+      navigate(from, { replace: true });
+    } catch (err) {
+      let errorMsg;
+      if (err.response) {
+        console.log(
+          `Response status: ${
+            err.response.status
+          } Response data: ${JSON.stringify(err.response.data)}`
+        );
+        errorMsg =
+          err.response.status === 401
+            ? "Username and password does not match"
+            : err.response?.data?.description;
+      } else {
+        console.log(`Error: ${err.message}`);
+        errorMsg = err.message;
+      }
+      setErrorMessage(errorMsg);
+    }
+  };
+
+  return (
+    <div className="loginArea">
+      <h1>Login to Proceed</h1>
+      <form className="loginForm" onSubmit={handleSubmit}>
+        <label htmlFor="username">Username</label>
+        <input
+          autoFocus
+          id="username"
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          id="password"
+          type="password"
+          value={pwd}
+          onChange={(e) => setPwd(e.target.value)}
+        />
+        {errorMessage && <FormPrompt message={`Failure - ${errorMessage}`} />}
+        <button disabled={!username || !pwd}>Login</button>
+      </form>
+      <Link to="/register">Register</Link>
+    </div>
+  );
+};
+
+export default LoginForm;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,50 +1,10 @@
-import React, { useState } from "react";
-import { useDispatch } from "react-redux";
-import { Link, useLocation, useNavigate } from "react-router-dom";
-import { userLogged } from "../store/authSlice";
+import React from "react";
+import LoginForm from "../components/login/LoginForm";
 
 const Login = () => {
-  const [username, setUsername] = useState("");
-  const [pwd, setPwd] = useState("");
-
-  const navigate = useNavigate();
-  const location = useLocation();
-  const from = location.state?.from?.pathname || "/"; // Navigate user back to wherever he/she was.
-
-  const dispatch = useDispatch();
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("Handle submit");
-    // todo - let's pretend this is success.
-    dispatch(userLogged({}))
-    navigate(from, { replace: true });
-  };
-
   return (
     <main className="loginComp">
-      <div className="loginArea">
-        <h1>Login to Proceed</h1>
-        <form className="loginForm" onSubmit={handleSubmit}>
-          <label htmlFor="username">Username</label>
-          <input
-            autoFocus
-            id="username"
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          <label htmlFor="password">Password</label>
-          <input
-            id="password"
-            type="password"
-            value={pwd}
-            onChange={(e) => setPwd(e.target.value)}
-          />
-          <button disabled={!username || !pwd}>Login</button>
-        </form>
-        <Link to="/register">Register</Link>
-      </div>
+      <LoginForm />
     </main>
   );
 };
