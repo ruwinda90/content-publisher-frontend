f8785a8e93744bd6b948307d7d8c2ff162be7bf8
ruwinda90 <ruwinda90@gmail.com>
2023-10-20 21:03:23 +0530

    Remove unwanted comments

--- src/components/notification/NotificationArea.jsx
+++ src/components/notification/NotificationArea.jsx
@@ -1,23 +1,11 @@
 import React, { useEffect, useState } from "react";
 import Portal from "./Portal";
-import Notification, { notifyType } from "./Notification";
-import { useDispatch, useSelector } from "react-redux";
-import { notificationSent } from "../../store/notificationSlice";
+import Notification from "./Notification";
+import { useSelector } from "react-redux";
 
 const NotificationArea = () => {
   const notifyMessage = useSelector((state) => state.notification.notify);
   const [messageQueue, setMessageQueue] = useState([]);
-  const dispatch = useDispatch();
-
-//   const sendNotify = () => {
-//     const arr = ["Mike", "Tyke", "Spike", "Tom", "Jerry"];
-//     dispatch(
-//       notificationSent({
-//         message: `Hello there Hello ${arr[Math.floor(Math.random() * arr.length)]}`,
-//         type: notifyType.ALERT,
-//       })
-//     );
-//   };
 
   useEffect(() => {
     setMessageQueue((state) => [...state, notifyMessage]);
@@ -41,7 +29,6 @@ const NotificationArea = () => {
             />
           )
       )}
-      {/* <button onClick={() => sendNotify()}>eps</button> */}
     </Portal>
   );
 };
