40da4f40e80f0ed6252baaf786e26ab3bfe49c4c
ruwinda90 <ruwinda90@gmail.com>
2023-10-20 21:37:40 +0530

    Remove json-server API mocks

--- src/api/apiRequest.js
+++ src/api/apiRequest.js
@@ -1,10 +1,5 @@
 import axios from "axios";
 
-export const api = axios.create({
-  baseURL: "http://localhost:3500",
-  timeout: 2000,
-});
-
 export const authApi = axios.create({
   baseURL: "http://localhost:8080/api/v1.0/auth",
   timeout: 2000,
